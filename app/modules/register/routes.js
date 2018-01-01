f56596f89eeaaf9f4187e29e30f83a2e2fcc8e90
Jon Ervin <balmacedajonervin@gmail.com>
2018-01-02 03:54:12 +0800

    register update (working register)

--- app/modules/register/routes.js
+++ app/modules/register/routes.js
@@ -2,14 +2,33 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../lib/database')();
 
+function auth(req,res,next){
+  db.query("SELECT * FROM tbluser WHERE strUserName= ?",[req.body.username], (err, results, fields) => {
+      if (err) console.log(err);
+      if(!results[0])
+        return next();
+      else {
+        res.render('register/views/invalid/taken');
+      }
+  });
+}
+
 function render(req,res){
   res.render('register/views/index');
 }
 
 router.get('/', render);
 
-router.post('/', (req, res) => {
-  res.redirect('/login');
+router.post('/', auth, (req, res) => {
+  if(req.body.password === req.body.confirm){
+    db.query("INSERT INTO tbluser (strName, strUserName, strPassword, intType, intStatus, strCity, strBarangay, strEmail, strContactNo) VALUES (?,?,?,'2','1',?,?,?,?)",[req.body.name, req.body.username, req.body.password, req.body.city, req.body.brngy, req.body.email, req.body.contact], (err, results, fields) => {
+      if (err) console.log(err);
+      res.render('register/views/success');
+    });
+  }
+  else{
+    res.render('register/views/invalid/notmatch');
+  }
 });
 
 exports.register = router;
