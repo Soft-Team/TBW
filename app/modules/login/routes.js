957f94d5b867e1660726d262a3a81ce5708c624c
Jon Ervin <balmacedajonervin@gmail.com>
2018-01-02 02:05:24 +0800

    login update (working login)

--- app/modules/login/routes.js
+++ app/modules/login/routes.js
@@ -3,13 +3,29 @@ var router = express.Router();
 var db = require('../../lib/database')();
 
 function render(req,res){
+  req.session.user = '';
   res.render('login/views/index');
 }
 
 router.get('/', render);
 
 router.post('/', (req, res) => {
-  res.redirect('/home');
+  db.query("SELECT * FROM tbluser WHERE strUserName= ?",[req.body.username], (err, results, fields) => {
+    if (err) console.log(err);
+    if (!results[0])
+      res.render('login/views/invalid/incorrect');
+    else if (results[0].intStatus == '2')
+      res.render('login/views/invalid/banned');
+    else if(req.body.password === results[0].strPassword){
+      req.session.user = results[0].intAccNo;
+      if(results[0].intType == '1')
+        res.redirect('/admin');
+      else
+        res.redirect('/home');
+    }
+    else
+      res.render('login/views/invalid/incorrect');
+  });
 });
 
 exports.login = router;
