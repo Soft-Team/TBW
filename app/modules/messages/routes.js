caeda5b1544d4264285b5f77ea61b247efb99d1c
Jon Ervin <balmacedajonervin@gmail.com>
2018-01-28 21:47:37 +0800

    Message initial + SQLDump Update

--- app/modules/messages/routes.js
+++ app/modules/messages/routes.js
@@ -2,10 +2,18 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../lib/database')();
 
-function render(req,res){
-  res.render('messages/views/index');
+function chat(req,res,next){
+  db.query("SELECT * FROM tblchat INNER JOIN tblmessage ON intChatID= intMessChatID INNER JOIN tblservice ON intChatServ= intServID INNER JOIN tbluser ON intAccNo= intServAccNo INNER JOIN tblservicetag ON intServTagID= intServTag WHERE intAccNo= 1", (err, results, fields) => {
+      if (err) console.log(err);
+      req.chat= results;
+      return next();
+    });
 }
 
-router.get('/', render);
+function render(req,res){
+  res.render('messages/views/index', { chattab: req.chat });
+}
+
+router.get('/:chatid', chat, render);
 
 exports.messages = router;
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 require('dotenv').config();
 var express = require('express');
 var winston = require('winston');
+var socket = require('socket.io');
 var app = express();
 require('./app')(app);
 
-app.listen(app.get('port'), () => {
+var server = app.listen(app.get('port'), () => {
     console.log(`ExpressJS server listening to port ${app.get('port')}`);
+});
+
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+        var db = require('./app/lib/database')();
+        db.query("INSERT INTO tblmessage ( message_chatid, message_sender, message_text ) VALUES ( 1, 1, ? )",[data.message], (err, results, fields) => {
+            if (err) console.log(err);
+          });
+    });
+
 });
