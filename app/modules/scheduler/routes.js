afcc4b9aaa7446ba0d036cf247c7beea523e1e47
JonErvin <balmacedajonervin@gmail.com>
2018-02-19 22:52:25 +0800

    Regular WH displayed

--- app/modules/scheduler/routes.js
+++ app/modules/scheduler/routes.js
@@ -2,6 +2,108 @@ var express = require('express');
 var router = express.Router();
 var db = require('../../lib/database')();
 var flog = require('../welcome/loggedin');
+var timeFormat = require('../welcome/timeFormat');
+
+function regularSched(req, res, next){
+  /*Regular Schedule of Current User
+  *(tblservicetag)*/
+  db.query("SELECT * FROM tblschedule WHERE intSchedAccNo= ?",[req.session.user], function (err, results, fields) {
+      if (err) return res.send(err);
+      if(!results[0]){
+        var empty = 1;
+      }
+      else{
+        var empty = 0;
+        for(count=0;count<results.length;count++){
+          results[count].formatstart = results[count].tmSchedStart.toString().substring(0, results[count].tmSchedStart.length - 3).concat((parseFloat(results[count].tmSchedStart).toString()) < 12 ? "AM": "PM");
+          results[count].formatend = results[count].tmSchedEnd.toString().substring(0, results[count].tmSchedEnd.length - 3).concat((parseFloat(results[count].tmSchedEnd).toString()) < 12 ? "AM": "PM");
+          console.log(("12:00:00").Parse);
+          switch (results[count].strSchedDay){
+            case "Sunday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].sunday = 1;
+              }
+              break;
+            case "Monday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].monday = 1;
+              }
+              break;
+            case "Tuesday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].tuesday = 1;
+              }
+              break;
+            case "Wednesday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].wednesday = 1;
+              }
+              break;
+            case "Thursday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].thursday = 1;
+              }
+              break;
+            case "Friday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].friday = 1;
+              }
+              break;
+            case "Saturday":
+              for(count1=0;count1<results.length;count1++){
+                results[count1].saturday = 1;
+              }
+              break;
+          }
+        }
+        if (!results[0].sunday){
+          for(count=0;count<results.length;count++){
+            results[count].sunday = 0;
+            results[results.length-1].sunday = 2;
+          }
+        }
+        if (!results[0].monday){
+          for(count=0;count<results.length;count++){
+            results[count].monday = 0;
+            results[results.length-1].monday = 2;
+          }
+        }
+        if (!results[0].tuesday){
+          for(count=0;count<results.length;count++){
+            results[count].tuesday = 0;
+            results[results.length-1].tuesday = 2;
+          }
+        }
+        if (!results[0].wednesday){
+          for(count=0;count<results.length;count++){
+            results[count].wednesday = 0;
+            results[results.length-1].wednesday = 2;
+          }
+        }
+        if (!results[0].thursday){
+          for(count=0;count<results.length;count++){
+            results[count].thursday = 0;
+            results[results.length-1].thursday = 2;
+          }
+        }
+        if (!results[0].friday){
+          for(count=0;count<results.length;count++){
+            results[count].friday = 0;
+            results[results.length-1].friday = 2;
+          }
+        }
+        if (!results[0].saturday){
+          for(count=0;count<results.length;count++){
+            results[count].saturday = 0;
+            results[results.length-1].saturday = 2;
+          }
+        }
+      }
+      req.regularSched = results;
+      req.empty = empty;
+      return next();
+  });
+}
 
 function render(req,res){
   switch (req.valid) {
@@ -10,11 +112,11 @@ function render(req,res){
       break;
     case 2:
     case 3:
-      res.render('scheduler/views/index', {thisUserTab: req.user});
+      res.render('scheduler/views/index', {thisUserTab: req.user, regSchedTab: req.regularSched, empty: req.empty});
       break;
   }
 }
 
-router.get('/', flog, render);
+router.get('/', flog, regularSched, render);
 
 exports.scheduler = router;
