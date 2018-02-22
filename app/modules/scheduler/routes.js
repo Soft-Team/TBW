f398917d596a235326e36dc027c3c517e948c27f
JonErvin <balmacedajonervin@gmail.com>
2018-02-23 03:33:25 +0800

    SWH Unav Update

--- app/modules/scheduler/routes.js
+++ app/modules/scheduler/routes.js
@@ -171,8 +171,14 @@ function specialSched(req, res, next){
       else{
         var empty = 0;
         for(count=0;count<results.length;count++){
-          results[count].formatstart = formatAMPM(results[count].tmSpecialStart);
-          results[count].formatend = formatAMPM(results[count].tmSpecialEnd);
+          if(!results[count].tmSpecialStart){
+            results[count].unav = 1;
+          }
+          else{
+            results[count].formatstart = formatAMPM(results[count].tmSpecialStart);
+            results[count].formatend = formatAMPM(results[count].tmSpecialEnd);
+            results[count].unav = 0;
+          }
           results[count].date = results[count].datSpecialDate.toDateString("en-US").slice(4, 15);
         }
       }
@@ -188,57 +194,63 @@ function specialDay(req, res, next){
       if (err) return res.send(err);
       if(!(!results[0])){
         var zero = "0";
-        var Hstart = results[0].tmSpecialStart.charAt(0).concat(results[0].tmSpecialStart.charAt(1));
-        var Hend = results[0].tmSpecialEnd.charAt(0).concat(results[0].tmSpecialEnd.charAt(1));
         var Sampm, Eampm;
         var formatDate = dateformat(results[0].datSpecialDate);
 
-        if (parseFloat(Hstart) > 11){
-            Sampm = 'PM';
+        if(!results[0].tmSpecialStart){
+          results[0].unav = 1;
         }
-        else {
-            Sampm = 'AM';
-        }
-        if (parseFloat(Hstart) > 12){
-            Hstart = (parseFloat(Hstart) - 12).toString();
-        }
-        if (parseFloat(Hstart) == 0){
-            Hstart = '12';
-            Sampm = 'AM';
-        }
-        if (Hstart.length == 1){
-            Hstart = zero.concat(Hstart);
-        }
+        else{
+          var Hstart = results[0].tmSpecialStart.charAt(0).concat(results[0].tmSpecialStart.charAt(1));
+          var Hend = results[0].tmSpecialEnd.charAt(0).concat(results[0].tmSpecialEnd.charAt(1));
 
-        if (parseFloat(Hend) > 11){
-            Eampm = 'PM';
-        }
-        else {
-            Eampm = 'AM';
-        }
-        if (parseFloat(Hend) > 12){
-            Hend = (parseFloat(Hend) - 12).toString();
-        }
-        if (parseFloat(Hend) == 0){
-            Hend = '12';
-            Eampm = 'AM';
-        }
-        if (Hend.length == 1){
-            Hend = zero.concat(Hend);
-        }
+          results[0].unav = 0;
 
-        results[0].Hstart = Hstart;
-        results[0].Hend = Hend;
-        results[0].Mstart = results[0].tmSpecialStart.charAt(3).concat(results[0].tmSpecialStart.charAt(4));
-        results[0].Mend = results[0].tmSpecialEnd.charAt(3).concat(results[0].tmSpecialEnd.charAt(4));
-        results[0].Sampm = Sampm;
-        results[0].Eampm = Eampm;
+          if (parseFloat(Hstart) > 11){
+              Sampm = 'PM';
+          }
+          else {
+              Sampm = 'AM';
+          }
+          if (parseFloat(Hstart) > 12){
+              Hstart = (parseFloat(Hstart) - 12).toString();
+          }
+          if (parseFloat(Hstart) == 0){
+              Hstart = '12';
+              Sampm = 'AM';
+          }
+          if (Hstart.length == 1){
+              Hstart = zero.concat(Hstart);
+          }
+
+          if (parseFloat(Hend) > 11){
+              Eampm = 'PM';
+          }
+          else {
+              Eampm = 'AM';
+          }
+          if (parseFloat(Hend) > 12){
+              Hend = (parseFloat(Hend) - 12).toString();
+          }
+          if (parseFloat(Hend) == 0){
+              Hend = '12';
+              Eampm = 'AM';
+          }
+          if (Hend.length == 1){
+              Hend = zero.concat(Hend);
+          }
+
+          results[0].Hstart = Hstart;
+          results[0].Hend = Hend;
+          results[0].Mstart = results[0].tmSpecialStart.charAt(3).concat(results[0].tmSpecialStart.charAt(4));
+          results[0].Mend = results[0].tmSpecialEnd.charAt(3).concat(results[0].tmSpecialEnd.charAt(4));
+          results[0].Sampm = Sampm;
+          results[0].Eampm = Eampm;
+        }
 
         results[0].month = formatDate.charAt(4).concat(formatDate.charAt(5));
         results[0].day = formatDate.charAt(6).concat(formatDate.charAt(7));
         results[0].year = formatDate.charAt(0) + formatDate.charAt(1) + formatDate.toString().charAt(2) + formatDate.toString().charAt(3);
-        console.log(formatDate);
-        console.log(results);
       }
       req.specialDay = results;
       return next();
@@ -372,47 +384,63 @@ router.post('/rwh/:schedid', flog, (req, res) => {
   });
 });
 router.post('/swh', flog, regularSched, specialSched, (req, res) => {
-  if (req.body.Sampm == 'AM' && req.body.Shours == '12'){
-      req.body.Shours = '00';
-  }
-  if (req.body.Eampm == 'AM' && req.body.Ehours == '12'){
-      req.body.Ehours = '00';
-  }
-  if (req.body.Sampm == 'PM' && req.body.Shours != '12'){
-    req.body.Shours = (parseFloat(req.body.Shours) + 12).toString();
-  }
-  if (req.body.Eampm == 'PM' && req.body.Ehours != '12'){
-    req.body.Ehours = (parseFloat(req.body.Ehours) + 12).toString();
-  }
-  var start = req.body.Shours.concat(':'+req.body.Sminutes);
-  var end = req.body.Ehours.concat(':'+req.body.Eminutes);
   var date = req.body.addYear.toString()+'-'+req.body.addMonth+'-'+req.body.addDay;
-  db.query("INSERT INTO tblspecialsched (intSpecialAccNo, datSpecialDate, tmSpecialStart, tmSpecialEnd) VALUES (?,?,?,?)",[req.session.user, date, start, end], (err, results, fields) => {
+  if (req.body.status == 'unavailable'){
+    var stringquery = "INSERT INTO tblspecialsched (intSpecialAccNo, datSpecialDate) VALUES (?,?)";
+    var bodyarray = [req.session.user, date];
+  }
+  else{
+    if (req.body.Sampm == 'AM' && req.body.Shours == '12'){
+        req.body.Shours = '00';
+    }
+    if (req.body.Eampm == 'AM' && req.body.Ehours == '12'){
+        req.body.Ehours = '00';
+    }
+    if (req.body.Sampm == 'PM' && req.body.Shours != '12'){
+      req.body.Shours = (parseFloat(req.body.Shours) + 12).toString();
+    }
+    if (req.body.Eampm == 'PM' && req.body.Ehours != '12'){
+      req.body.Ehours = (parseFloat(req.body.Ehours) + 12).toString();
+    }
+    var start = req.body.Shours.concat(':'+req.body.Sminutes);
+    var end = req.body.Ehours.concat(':'+req.body.Eminutes);
+    var stringquery = "INSERT INTO tblspecialsched (intSpecialAccNo, datSpecialDate, tmSpecialStart, tmSpecialEnd) VALUES (?,?,?,?)";
+    var bodyarray = [req.session.user, date, start, end];
+  }
+  db.query(stringquery, bodyarray, (err, results, fields) => {
     if (err) res.render('scheduler/views/invalid/nodate', {thisUserTab: req.user, regSchedTab: req.regularSched, empty: req.empty, specSchedTab: req.specialSched, emptyspecial: req.emptyspecial});
     res.redirect('/scheduler');
   });
 });
 router.post('/swh/:schedid', flog, (req, res) => {
-  if (req.body.ESampm == 'AM' && req.body.EShours == '12'){
-      req.body.EShours = '00';
-  }
-  if (req.body.EEampm == 'AM' && req.body.EEhours == '12'){
-      req.body.EEhours = '00';
-  }
-  if (req.body.ESampm == 'PM' && req.body.EShours != '12'){
-      req.body.EShours = (parseFloat(req.body.EShours) + 12).toString();
-  }
-  if (req.body.EEampm == 'PM' && req.body.EEhours != '12'){
-      req.body.EEhours = (parseFloat(req.body.EEhours) + 12).toString();
-  }
-  var start = req.body.EShours.concat(':'+req.body.ESminutes);
-  var end = req.body.EEhours.concat(':'+req.body.EEminutes);
   var date = req.body.addYear.toString()+'-'+req.body.addMonth+'-'+req.body.addDay;
-  db.query("UPDATE tblspecialsched SET datSpecialDate= ?, tmSpecialStart= ?, tmSpecialEnd= ? WHERE intSpecialID= ?",[date, start, end, req.params.schedid], (err, results, fields) => {
+  if (req.body.status == 'unavailable'){
+    var stringquery = "UPDATE tblspecialsched SET datSpecialDate= ?, tmSpecialStart=NULL, tmSpecialEnd=NULL WHERE intSpecialID= ?";
+    var bodyarray = [date, req.params.schedid];
+  }
+  else{
+    if (req.body.ESampm == 'AM' && req.body.EShours == '12'){
+        req.body.EShours = '00';
+    }
+    if (req.body.EEampm == 'AM' && req.body.EEhours == '12'){
+        req.body.EEhours = '00';
+    }
+    if (req.body.ESampm == 'PM' && req.body.EShours != '12'){
+        req.body.EShours = (parseFloat(req.body.EShours) + 12).toString();
+    }
+    if (req.body.EEampm == 'PM' && req.body.EEhours != '12'){
+        req.body.EEhours = (parseFloat(req.body.EEhours) + 12).toString();
+    }
+    var start = req.body.EShours.concat(':'+req.body.ESminutes);
+    var end = req.body.EEhours.concat(':'+req.body.EEminutes);
+    var stringquery = "UPDATE tblspecialsched SET datSpecialDate= ?, tmSpecialStart= ?, tmSpecialEnd= ? WHERE intSpecialID= ?";
+    var bodyarray = [date, start, end, req.params.schedid];
+  }
+  console.log(bodyarray);
+  db.query(stringquery, bodyarray, (err, results, fields) => {
     if (err) console.log(err);
     res.redirect('/scheduler');
   });
 });
-
 
 exports.scheduler = router;
