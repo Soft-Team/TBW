85346928f20b64a845f6c8b2df2f44555ba1c206
JonErvin <balmacedajonervin@gmail.com>
2018-02-27 05:40:56 +0800

    Search Name

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('../../lib/database')();
 var flog = require('../welcome/loggedin');
 var messCount = require('../welcome/messCount');
+var prepend = require('../welcome/prepend');
 
 function render(req,res){
   switch (req.valid) {
@@ -66,7 +67,22 @@ function searchRender(req,res){
       break;
     case 2:
     case 3:
-      res.render('home/views/search', {thisUserTab: req.user, messCount: req.messCount[0].count});
+      var searchquery = "%"+req.params.searchname.toString()+"%"
+      db.query("SELECT * FROM tbluser WHERE strName LIKE ? AND intStatus!= 3 AND intType!= 1",[searchquery], (err, results, fields) => {
+          if (err) console.log(err);
+          if(!(!results[0])){
+            for(count=0;count<results.length;count++){
+              if (results[count].intType == 3 && results[count].intStatus == 1){
+                results[count].intAccNo = 0;
+              }
+              results[count].prepend = prepend(results[count].intAccNo);
+            }
+            res.render('home/views/search', {thisUserTab: req.user, messCount: req.messCount[0].count, searchtab: results});
+          }
+          else{
+            res.render('home/views/nosearch', {thisUserTab: req.user, messCount: req.messCount[0].count, noresult: req.params.searchname});
+          }
+      });
       break;
   }
 }
@@ -76,6 +92,10 @@ router.get('/guide', flog, messCount, guideRender);
 router.get('/help', flog, messCount, helpRender);
 router.get('/about', flog, messCount, aboutRender);
 router.get('/team', flog, messCount, teamRender);
-router.get('/search', flog, messCount, searchRender);
+router.get('/search/:searchname', flog, messCount, searchRender);
+
+router.post('/search', flog, (req, res) => {
+  res.redirect('/home/search/'+req.body.search);
+});
 
 exports.home = router;
