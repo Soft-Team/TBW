b376f42e70473601a3348fa9eb41ed7bbee910ca
9Weisss <otaku0146@yahoo.com>
2018-02-27 02:38:22 +0800

    Update

--- app/modules/home/routes.js
+++ app/modules/home/routes.js
@@ -37,9 +37,33 @@ function helpRender(req,res){
       break;
   }
 }
+function aboutRender(req,res){
+  switch (req.valid) {
+    case 1:
+      res.render('welcome/views/invalid/adm-restrict');
+      break;
+    case 2:
+    case 3:
+      res.render('home/views/about', {thisUserTab: req.user, messCount: req.messCount[0].count});
+      break;
+  }
+}
+function teamRender(req,res){
+  switch (req.valid) {
+    case 1:
+      res.render('welcome/views/invalid/adm-restrict');
+      break;
+    case 2:
+    case 3:
+      res.render('home/views/team', {thisUserTab: req.user, messCount: req.messCount[0].count});
+      break;
+  }
+}
 
 router.get('/', flog, messCount, render);
 router.get('/guide', flog, messCount, guideRender);
 router.get('/help', flog, messCount, helpRender);
+router.get('/about', flog, messCount, aboutRender);
+router.get('/team', flog, messCount, teamRender);
 
 exports.home = router;
